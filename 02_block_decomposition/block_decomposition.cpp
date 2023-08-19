8053a30f386cd5ba40123f8acbef542ab9e87f02
Tony Fu <61669380+tonyfu97@users.noreply.github.com>
2023-08-19 16:31:08 -0700

    Finish chapter 2

--- 02_block_decomposition/block_decomposition.cpp
+++ 02_block_decomposition/block_decomposition.cpp
@@ -32,6 +32,7 @@ int main(int argc, char **argv)
 
     // Get the gradient norm of the luminance
     CImgList<> grad = lum.get_gradient("xy");
+    grad.display();
     CImg<> normGrad = (grad[0].get_sqr() += grad[1].get_sqr()).sqrt();
     normGrad.save(("results/" + file_basename + "_normGrad.png").c_str());
 
@@ -46,7 +47,8 @@ int main(int argc, char **argv)
         int
             x0 = block[0],
             y0 = block[1],
-            x1 = block[2], y1 = block[3];
+            x1 = block[2],
+            y1 = block[3];
         if (normGrad.get_crop(x0, y0, x1, y1).max() > threshold &&
             std::min(x1 - x0, y1 - y0) > 8)
         {
@@ -70,19 +72,27 @@ int main(int argc, char **argv)
         int
             x0 = block[0],
             y0 = block[1],
-            x1 = block[2], y1 = block[3];
+            x1 = block[2],
+            y1 = block[3];
         CImg<unsigned char> color = img.get_crop(x0, y0, x1, y1).resize(1, 1, 1, 3, 2);
         res.draw_rectangle(x0, y0, x1, y1, color.data(), 1);
         coords.draw_rectangle(x0, y0, x1, y1, block.data());
     }
 
     // Adding black borders.
-    res.mul(1 - (res.get_shift(1, 1, 0, 0, 0) - res).norm().cut(0, 1));
+    CImg<unsigned char> mask = 1 - (res.get_shift(1, 1, 0, 0, 0) - res).norm().cut(0, 1);
+    
+    mask.get_normalize(0, 255).save(("results/" + file_basename + "_mask.png").c_str());
+    res.save(("results/" + file_basename + "_res.png").c_str());
+    res.mul(mask);
     res.save(("results/" + file_basename + "_blocks.png").c_str());
 
     // Start the interactive viewer.
     CImgDisplay disp(res, "CImg Tutorial: Block Decomposition", 0);
+
+    // Define colors.
     unsigned char white[] = {255, 255, 255}, black[] = {0, 0, 0};
+
     while (!disp.is_closed() && !disp.is_keyESC())
     {
         int
@@ -90,15 +100,29 @@ int main(int argc, char **argv)
             y = disp.mouse_y();
         if (x >= 0 && y >= 0)
         {
+            // Get the coordinates of the block under the mouse position.
             int
                 x0 = coords(x, y, 0),
                 y0 = coords(x, y, 1),
                 x1 = coords(x, y, 2), y1 = coords(x, y, 3),
                 xc = (x0 + x1) / 2, yc = (y0 + y1) / 2;
+
+            // Get the block and its gradient.
             CImg<unsigned char>
                 pImg = img.get_crop(x0, y0, x1, y1).resize(128, 128, 1, 3, 1),
                 pGrad = normGrad.get_crop(x0, y0, x1, y1).resize(128, 128, 1, 3, 1).normalize(0, 255).map(CImg<unsigned char>::hot_LUT256());
-            (+res).draw_text(10, 3, "X, Y = %d, %d", white, 0, 1, 24, x, y).draw_rectangle(x0, y0, x1, y1, black, 0.25f).draw_line(74, 109, xc, yc, white, 0.75, 0xCCCCCCCC).draw_line(74, 264, xc, yc, white, 0.75, 0xCCCCCCCC).draw_rectangle(7, 32, 140, 165, white).draw_rectangle(7, 197, 140, 330, white).draw_image(10, 35, pImg).draw_image(10, 200, pGrad).display(disp);
+
+            // Display the block and its gradient.
+            (+res).
+            draw_text(10, 3, "X, Y = %d, %d", white, 0, 1, 24, x, y).
+            draw_rectangle(x0, y0, x1, y1, black, 0.25f).
+            draw_line(74, 109, xc, yc, white, 0.75, 0xCCCCCCCC).
+            draw_line(74, 264, xc, yc, white, 0.75, 0xCCCCCCCC).
+            draw_rectangle(7, 32, 140, 165, white).
+            draw_rectangle(7, 197, 140, 330, white).
+            draw_image(10, 35, pImg).
+            draw_image(10, 200, pGrad).
+            display(disp);
         }
         disp.wait(); // Wait for an user event
         if (disp.is_resized())
