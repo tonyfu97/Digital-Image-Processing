f399967125c2fd6a5f65ad90b80688000ae2b535
Tony Fu <61669380+tonyfu97@users.noreply.github.com>
2023-08-28 23:57:09 -0700

    Deployed 162b21f with MkDocs version: 1.4.3

--- images/gif_2_png.py
+++ images/gif_2_png.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageSequence
 
-img = Image.open("driveby.gif")
+img = Image.open("shuffleboard.gif")
 for i, frame in enumerate(ImageSequence.Iterator(img)):
-    frame.save(f"driveby_{i}.png")
+    frame.save(f"shuffleboard_{i}.png")
